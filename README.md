ce24a2f57ba86a19d6e24c3628ffa593ff817761
aoakenfo <ash.oakenfold@gmail.com>
2013-12-15 11:40:02 -0800

    lesson 6: point vertex buffer

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ hello-web-gl
 Learning WebGL and Dart.
 
 Each commit corresponds to an incremental lesson.
+
+A mirror of examples found in the excellent book [WebGL Programming Guide: Interactive 3D Graphics Programming with WebGL (OpenGL)](http://www.amazon.com/WebGL-Programming-Guide-Interactive-Graphics-ebook/dp/B00DS74080/ref=tmm_kin_title_0)
--- web/hello_web_gl.dart
+++ web/hello_web_gl.dart
@@ -1,5 +1,6 @@
 import 'dart:html';
 import 'dart:web_gl';
+import 'dart:typed_data';
 
 String vertexShaderSource = '''
 attribute vec4 a_Position;
@@ -36,52 +37,25 @@ void main() {
   gl.linkProgram(program);
   gl.useProgram(program);
   
+  var vertices = new Float32List.fromList([
+    0.0,  0.5,
+   -0.5, -0.5,
+    0.5, -0.5
+  ]);
+  
+  Buffer vertexBuffer = gl.createBuffer();
+  gl.bindBuffer(ARRAY_BUFFER, vertexBuffer);
+  gl.bufferDataTyped(ARRAY_BUFFER, vertices, STATIC_DRAW);
+  
   int a_Position = gl.getAttribLocation(program, 'a_Position');
+  gl.vertexAttribPointer(a_Position, 2, FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(a_Position);
+  
   UniformLocation u_FragColor = gl.getUniformLocation(program, 'u_FragColor');
-  
-  var points = new List<Point>();
-  var colors = [
-    [1.0, 0.0, 0.0, 1.0],
-    [0.0, 1.0, 0.0, 1.0],
-    [0.0, 0.0, 1.0, 1.0]
-  ];
-  
-  void onMouseDown(MouseEvent event) {
-    num x = event.client.x;
-    num y = event.client.y;
-    
-    // remove any default styling offset applied to canvas position
-    x -= canvas.offsetLeft;
-    y -= canvas.offsetTop;
-    
-    // translate to center
-    x -= (canvas.width  / 2);
-    y -= (canvas.height / 2);
-    
-    // scale 0-1
-    x /=  (canvas.width  / 2);
-    y /= -(canvas.height / 2); // flip y-axis
-    
-    points.add(new Point(x, y));
-    
-    gl.clear(COLOR_BUFFER_BIT);
-    
-    int idx = -1;
-    var rgba = null;
-    for(int i = 0; i < points.length; ++i) {
-      gl.vertexAttrib3f(a_Position, points[i].x, points[i].y, 0.0);
-      
-      idx = ++idx % colors.length;
-      rgba = colors[idx];
-      gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-      
-      // ignore the warning "Attribute 0 is disabled. This has signficant performance penalty"
-      gl.drawArrays(POINTS, 0, 1);
-    }
-  }
-  
-  canvas.onMouseDown.listen((e) => onMouseDown(e) );
+  gl.uniform4f(u_FragColor, 1.0, 0.0, 0.0, 1.0);
   
   gl.clearColor(0.5, 0.5, 0.5, 1.0);
   gl.clear(COLOR_BUFFER_BIT);
+
+  gl.drawArrays(POINTS, 0, vertices.length ~/ 2); 
 }
