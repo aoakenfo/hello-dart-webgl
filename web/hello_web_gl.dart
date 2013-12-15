b257a70d3a38eaa0f86d7ab3255bf897331c56ab
aoakenfo <ash.oakenfold@gmail.com>
2013-12-15 00:40:01 -0800

    lesson 3: attribute and uniform

--- web/hello_web_gl.dart
+++ web/hello_web_gl.dart
@@ -2,15 +2,18 @@ import 'dart:html';
 import 'dart:web_gl';
 
 String vertexShaderSource = '''
+attribute vec4 a_Position;
 void main() {
-  gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+  gl_Position = a_Position;
   gl_PointSize = 10.0;
 }
 ''';
 
 String fragmentShaderSource = '''
+precision mediump float;
+uniform vec4 u_FragColor;
 void main() {
-  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  gl_FragColor = u_FragColor;
 }
 ''';
 
@@ -32,6 +35,12 @@ void main() {
   gl.attachShader(program, fragmentShader);
   gl.linkProgram(program);
   gl.useProgram(program);
+  
+  int a_Position = gl.getAttribLocation(program, 'a_Position');
+  gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
+  
+  UniformLocation u_FragColor = gl.getUniformLocation(program, 'u_FragColor');
+  gl.uniform4f(u_FragColor, 1.0, 0.0, 0.0, 1.0);
   
   gl.clearColor(0.5, 0.5, 0.5, 1.0);
   gl.clear(COLOR_BUFFER_BIT);
