47ed40994d3ed7ec19b89d2e42d79f39a0e52754
aoakenfo <ash.oakenfold@gmail.com>
2013-12-18 00:39:48 -0800

    exercise 14: rotated triangle using vector_math package

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ hello-web-gl
 
 Learning WebGL and Dart.
 
-Each commit corresponds to an incremental lesson.
+Each commit corresponds to an incremental exercise.
 
 Based on examples found in the excellent book [WebGL Programming Guide: Interactive 3D Graphics Programming with WebGL (OpenGL)](http://www.amazon.com/WebGL-Programming-Guide-Interactive-Graphics-ebook/dp/B00DS74080/ref=tmm_kin_title_0)
--- web/hello_web_gl.dart
+++ web/hello_web_gl.dart
@@ -2,6 +2,7 @@ import 'dart:html';
 import 'dart:web_gl';
 import 'dart:typed_data';
 import 'dart:math';
+import 'package:vector_math/vector_math.dart';
 
 String vertexShaderSource = '''
 attribute vec4 a_Position;
@@ -18,9 +19,6 @@ void main() {
   gl_FragColor = u_FragColor;
 }
 ''';
-
-var tick;
-var animate = (num highResTime) => tick(highResTime);
 
 void main() {
   
@@ -55,95 +53,18 @@ void main() {
   gl.vertexAttribPointer(a_Position, 2, FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(a_Position);
   
+  Matrix4 modelMatrix = new Matrix4.identity()
+    ..rotateZ(-PI / 2)
+    ..translate(0.0, 1.0, 0.0);
+  
   UniformLocation u_ModelMatrix = gl.getUniformLocation(program, 'u_ModelMatrix');
+  gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.storage);
+  
   UniformLocation u_FragColor = gl.getUniformLocation(program, 'u_FragColor');
   gl.uniform4f(u_FragColor, 1.0, 0.0, 0.0, 1.0);
   
   gl.clearColor(0.5, 0.5, 0.5, 1.0);
-
-  num radian = 0.0;
-  num sinB = 0.0;
-  num cosB = 0.0;
-  var modelMatrix = new Float32List.fromList([
-       1.0, 0.0, 0.0, 0.0,
-       0.0, 1.0, 0.0, 0.0,
-       0.0, 0.0, 1.0, 0.0,
-       0.0, 0.0, 0.0, 1.0
-  ]);
+  gl.clear(COLOR_BUFFER_BIT);
   
-  num lastTime = 0.0;
-  num angle = 0.0;
-  num speed = 40.0;
-  num direction = 1;
-  
-  ButtonElement leftButton = new ButtonElement();
-  ButtonElement rightButton = new ButtonElement();
-  ButtonElement plusButton = new ButtonElement();
-  ButtonElement minusButton = new ButtonElement();
-  DivElement div = new DivElement();
-  
-  div.children.add(leftButton);
-  div.children.add(rightButton);
-  div.children.add(plusButton);
-  div.children.add(minusButton);
-  document.body.children.insert(0, div);
-  
-  leftButton
-    ..disabled = true
-    ..text = 'left'
-    ..onClick.listen((MouseEvent e) {
-        direction *= -1;
-        leftButton.disabled = true;
-        rightButton.disabled = false;
-    });
-
-  rightButton
-    ..text = 'right'
-    ..onClick.listen((MouseEvent e) {
-      direction *= -1;
-      leftButton.disabled = false;
-      rightButton.disabled = true;
-    });
-  
-  plusButton
-    ..text = '+'
-    ..onClick.listen((MouseEvent e) {
-      speed += 10;
-      minusButton.disabled = false;
-    });
-  
-  minusButton
-    ..text = '-'
-    ..onClick.listen((MouseEvent e) {
-      speed -= 10;
-      if(speed <= 0) {
-        minusButton.disabled = true;  
-      }
-    });
-  
-  tick = (num highResTime) {
-      
-      window.requestAnimationFrame(animate);
-      
-      num elapsedTime = highResTime - lastTime;
-      lastTime = highResTime;
-      
-      angle += direction * (speed * elapsedTime / 1000.0);
-      angle %= 360.0;
-      radian = PI * angle / 180.0;
-      sinB = sin(radian);
-      cosB = cos(radian);
-      
-      modelMatrix[0] =  cosB;
-      modelMatrix[1] =  sinB;
-      modelMatrix[4] = -sinB;
-      modelMatrix[5] =  cosB;
-      
-      gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix);
-      gl.clear(COLOR_BUFFER_BIT);
-      gl.drawArrays(TRIANGLES, 0, 3);
-      
-    };
-    
-    animate(0);
+  gl.drawArrays(TRIANGLES, 0, 3);
 }
