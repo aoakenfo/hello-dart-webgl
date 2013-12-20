6c8fd56f736a8bb7714359ee55e9bc4911b7eba9
aoakenfo <ash.oakenfold@gmail.com>
2013-12-19 20:31:27 -0800

    exercise 16: textured quad

--- web/hello_web_gl.dart
+++ web/hello_web_gl.dart
@@ -4,20 +4,20 @@ import 'dart:typed_data';
 
 String vertexShaderSource = '''
 attribute vec4 a_Position;
-attribute vec4 a_Color;
-varying vec4 v_Color;
+attribute vec2 a_TexCoord;
+varying vec2 v_TexCoord;
 void main() {
   gl_Position = a_Position;
-  v_Color = a_Color;
+  v_TexCoord = a_TexCoord;
 }
 ''';
 
 String fragmentShaderSource = '''
 precision mediump float;
-varying vec4 v_Color;
-uniform vec4 u_FragColor;
+varying vec2 v_TexCoord;
+uniform sampler2D u_Sampler;
 void main() {
-  gl_FragColor = v_Color;
+  gl_FragColor = texture2D(u_Sampler, v_TexCoord);
 }
 ''';
 
@@ -41,9 +41,10 @@ void main() {
   gl.useProgram(program);
   
   var vertices = new Float32List.fromList([
-    -0.5, -0.5, 1.0, 0.0, 0.0,
-     0.0,  0.5, 0.0, 1.0, 0.0,
-     0.5, -0.5, 0.0, 0.0, 1.0
+    -0.5,  0.5,   0.0, 1.0,
+    -0.5, -0.5,   0.0, 0.0,
+     0.5,  0.5,   1.0, 1.0,
+     0.5, -0.5,   1.0, 0.0
   ]);
   
   Buffer vertexBuffer = gl.createBuffer();
@@ -52,16 +53,35 @@ void main() {
   
   int a_Position = gl.getAttribLocation(program, 'a_Position');
   gl.vertexAttribPointer(a_Position, 2, FLOAT, false, 
-      vertices.elementSizeInBytes * 5, 0);
+      vertices.elementSizeInBytes * 4, 0);
   gl.enableVertexAttribArray(a_Position);
  
-  int a_Color = gl.getAttribLocation(program, 'a_Color');
-  gl.vertexAttribPointer(a_Color, 3, FLOAT, false, 
-      vertices.elementSizeInBytes * 5, vertices.elementSizeInBytes * 2);
-  gl.enableVertexAttribArray(a_Color);
+  int a_TexCoord = gl.getAttribLocation(program, 'a_TexCoord');
+  gl.vertexAttribPointer(a_TexCoord, 2, FLOAT, false,
+      vertices.elementSizeInBytes * 4, vertices.elementSizeInBytes * 2);
+  gl.enableVertexAttribArray(a_TexCoord);
+  
+  // to load textures from disk:
+  //  Run menu -> Manage Launches
+  //  Browser arguments: --allow-file-access-from-files
+  Texture texture = gl.createTexture();
+  
+  ImageElement image = new ImageElement(src:'k.png');
+  image.onLoad.listen((e) {
+    gl.pixelStorei(UNPACK_FLIP_Y_WEBGL, 1);
+    
+    gl.activeTexture(TEXTURE0);
+    gl.bindTexture(TEXTURE_2D, texture);
+    
+    gl.texParameteri(TEXTURE_2D, TEXTURE_MIN_FILTER, LINEAR);
+    gl.texImage2D(TEXTURE_2D, 0, RGB, RGB, UNSIGNED_BYTE, image);
+    
+    UniformLocation u_Sampler = gl.getUniformLocation(program, 'u_Sampler');
+    gl.uniform1i(u_Sampler, 0);
+
+    gl.clear(COLOR_BUFFER_BIT);
+    gl.drawArrays(TRIANGLE_STRIP, 0, 4);
+  });
   
   gl.clearColor(0.5, 0.5, 0.5, 1.0);
-  gl.clear(COLOR_BUFFER_BIT);
-  
-  gl.drawArrays(TRIANGLES, 0, 3);
 }
