aa0c64c9fe35e399fabda031f2fe13ebf8521bbb
aoakenfo <ash.oakenfold@gmail.com>
2014-01-11 23:46:22 -0800

    exercise 19: perspective projection

--- web/hello_web_gl.dart
+++ web/hello_web_gl.dart
@@ -1,15 +1,16 @@
 import 'dart:html';
 import 'dart:web_gl';
 import 'dart:typed_data';
+import 'dart:math';
 import 'package:vector_math/vector_math.dart';
 
 String vertexShaderSource = '''
 attribute vec4 a_Position;
 attribute vec4 a_Color;
-uniform mat4 u_ProjMatrix;
+uniform mat4 u_MvpMatrix;
 varying vec4 v_Color;
 void main() {
-  gl_Position = u_ProjMatrix * a_Position;
+  gl_Position = u_MvpMatrix * a_Position;
   v_Color = a_Color;
 }
 ''';
@@ -42,17 +43,19 @@ void main() {
   gl.useProgram(program);
   
   var vertices = new Float32List.fromList([
-    0.0,  0.6,  -0.3,  1.0,  0.0,  0.0, // red triangle
-   -0.5, -0.4,  -0.3,  1.0,  0.0,  0.0,
-    0.5, -0.4,  -0.3,  1.0,  0.0,  0.0, 
+    // position         // color
+    0.0,  1.0,  0.0,    0.0,  0.0,  1.0, // front blue triangle 
+   -0.5, -1.0,  0.0,    0.0,  0.0,  1.0,
+    0.5, -1.0,  0.0,    0.0,  0.0,  1.0, 
+    
+    0.0,  1.0, -2.0,    1.0,  1.0,  0.0, // middle yellow triangle
+   -0.5, -1.0, -2.0,    1.0,  1.0,  0.0,
+    0.5, -1.0, -2.0,    1.0,  1.0,  0.0, 
 
-    0.5,  0.4,  -0.2,  0.0,  1.0,  0.0, // green triangle
-   -0.5,  0.4,  -0.2,  0.0,  1.0,  0.0,
-    0.0, -0.6,  -0.2,  0.0,  1.0,  0.0, 
+    0.0,  1.0, -4.0,    0.0,  1.0,  0.0, // back green triangle
+   -0.5, -1.0, -4.0,    0.0,  1.0,  0.0,
+    0.5, -1.0, -4.0,    0.0,  1.0,  0.0
 
-    0.0,  0.5,  -0.1,  0.0,  0.0,  1.0, // blue triangle 
-   -0.5, -0.5,  -0.1,  0.0,  0.0,  1.0,
-    0.5, -0.5,  -0.1,  0.0,  0.0,  1.0, 
   ]);
   
   Buffer vertexBuffer = gl.createBuffer();
@@ -69,47 +72,34 @@ void main() {
       vertices.elementSizeInBytes * 6, vertices.elementSizeInBytes * 3);
   gl.enableVertexAttribArray(a_Color);
   
-  UniformLocation u_ProjMatrix = gl.getUniformLocation(program, 'u_ProjMatrix');
+  num fovYRadians = PI / 4;
+  num aspectRatio = canvas.width / canvas.height;
+  num zNear = 1.0;
+  num zFar = 10.0;
+  Matrix4 projMatrix = makePerspectiveMatrix(fovYRadians, aspectRatio, zNear, zFar);
   
-  num farPlane = 0.3;
-  Function draw = () {
+  Vector3 cameraPosition = new Vector3(0.0, 0.0, 5.0);
+  Vector3 cameraFocusPosition = new Vector3(0.0, 0.0, -100.0);
+  Vector3 upDirection = new Vector3(0.0, 1.0, 0.0);
+  Matrix4 viewMatrix = makeViewMatrix(cameraPosition, cameraFocusPosition, upDirection);
+  
+  Matrix4 modelMatrix = new Matrix4.identity();
+  modelMatrix.setTranslation(new Vector3(-1.0, 0.0, 0.0));
+  
+  Matrix4 mvpMatrix = projMatrix * viewMatrix * modelMatrix;
+  UniformLocation u_MvpMatrix = gl.getUniformLocation(program, 'u_MvpMatrix');
+  gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.storage);
 
-    Matrix4 orthoMatrix = makeOrthographicMatrix(-1.0, 1.0, -1.0, 1.0, 0.0, farPlane);
-    gl.uniformMatrix4fv(u_ProjMatrix, false, orthoMatrix.storage);
-    
-    gl.clear(COLOR_BUFFER_BIT);
-    gl.drawArrays(TRIANGLES, 0, vertices.length ~/ 6);
-    
-  };
-  
-  ButtonElement plusButton  = new ButtonElement()
-    ..text = '+'
-    ..disabled = true;
-  ButtonElement minusButton = new ButtonElement()..text = '-';
-  DivElement div = new DivElement();
-  div.children.addAll([plusButton, minusButton]);
-  document.body.children.add(div);
-  
-  plusButton.onClick.listen((_) {
-      farPlane += 0.1;
-      if(farPlane >= 0.3) {
-        farPlane = 0.3;
-        plusButton.disabled = true;
-      }
-      minusButton.disabled = false;
-      draw();
-    });
-  
-  minusButton.onClick.listen((_) {
-    farPlane -= 0.1;
-    if(farPlane <= 0.0) {
-      farPlane = 0.0;
-      minusButton.disabled = true;
-    }
-    plusButton.disabled = false;
-    draw();
-  });
+  // try commenting out depth test
+  gl.enable(DEPTH_TEST);
   
   gl.clearColor(0.5, 0.5, 0.5, 1.0);
-  draw();
+  
+  gl.clear(COLOR_BUFFER_BIT | DEPTH_BUFFER_BIT);
+  gl.drawArrays(TRIANGLES, 0, vertices.length ~/ 6);
+
+  modelMatrix.setTranslation(new Vector3(1.0, 0.0, 0.0));
+  mvpMatrix = projMatrix * viewMatrix * modelMatrix;
+  gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.storage);
+  gl.drawArrays(TRIANGLES, 0, vertices.length ~/ 6);
 }
