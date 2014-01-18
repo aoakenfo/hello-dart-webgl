e18119cb656024b14a127b6f32881553ef1c6fb1
aoakenfo <ash.oakenfold@gmail.com>
2014-01-18 12:30:03 -0800

    exercise 23: cube with colored faces

--- web/hello_web_gl.dart
+++ web/hello_web_gl.dart
@@ -50,43 +50,53 @@ void main() {
   //  |/      |/
   //  v2------v3
   var vertices = new Float32List.fromList([
-     // position          // color
-     1.0,  1.0,  1.0,     1.0,  1.0,  1.0,  // v0 white
-    -1.0,  1.0,  1.0,     1.0,  0.0,  1.0,  // v1 magenta
-    -1.0, -1.0,  1.0,     1.0,  0.0,  0.0,  // v2 red
-     1.0, -1.0,  1.0,     1.0,  1.0,  0.0,  // v3 yellow
-     1.0, -1.0, -1.0,     0.0,  1.0,  0.0,  // v4 green
-     1.0,  1.0, -1.0,     0.0,  1.0,  1.0,  // v5 cyan
-    -1.0,  1.0, -1.0,     0.0,  0.0,  1.0,  // v6 blue
-    -1.0, -1.0, -1.0,     0.0,  0.0,  0.0   // v7 black 
+     1.0, 1.0, 1.0,  -1.0, 1.0, 1.0,  -1.0,-1.0, 1.0,   1.0,-1.0, 1.0,  // v0-v1-v2-v3 front
+     1.0, 1.0, 1.0,   1.0,-1.0, 1.0,   1.0,-1.0,-1.0,   1.0, 1.0,-1.0,  // v0-v3-v4-v5 right
+     1.0, 1.0, 1.0,   1.0, 1.0,-1.0,  -1.0, 1.0,-1.0,  -1.0, 1.0, 1.0,  // v0-v5-v6-v1 up
+    -1.0, 1.0, 1.0,  -1.0, 1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0,-1.0, 1.0,  // v1-v6-v7-v2 left
+    -1.0,-1.0,-1.0,   1.0,-1.0,-1.0,   1.0,-1.0, 1.0,  -1.0,-1.0, 1.0,  // v7-v4-v3-v2 down
+     1.0,-1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0, 1.0,-1.0,   1.0, 1.0,-1.0   // v4-v7-v6-v5 back 
+  ]);
+  
+  var colors = new Float32List.fromList([
+    0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  // v0-v1-v2-v3 front(blue)
+    0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  // v0-v3-v4-v5 right(green)
+    1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  // v0-v5-v6-v1 up(red)
+    1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  // v1-v6-v7-v2 left
+    1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  // v7-v4-v3-v2 down
+    0.4, 1.0, 1.0,  0.4, 1.0, 1.0,  0.4, 1.0, 1.0,  0.4, 1.0, 1.0   // v4-v7-v6-v5 back
   ]);
   
   var indices = new Uint8List.fromList([
-     0, 1, 2,   0, 2, 3,  // front
-     0, 3, 4,   0, 4, 5,  // right
-     0, 5, 6,   0, 6, 1,  // up
-     1, 6, 7,   1, 7, 2,  // left
-     7, 4, 3,   7, 3, 2,  // down
-     4, 7, 6,   4, 6, 5   // back
+     0, 1, 2,   0, 2, 3,    // front
+     4, 5, 6,   4, 6, 7,    // right
+     8, 9,10,   8,10,11,    // up
+    12,13,14,  12,14,15,    // left
+    16,17,18,  16,18,19,    // down
+    20,21,22,  20,22,23     // back
   ]);
   
   Buffer vertexBuffer = gl.createBuffer();
   gl.bindBuffer(ARRAY_BUFFER, vertexBuffer);
   gl.bufferDataTyped(ARRAY_BUFFER, vertices, STATIC_DRAW);
+
+  int a_Position = gl.getAttribLocation(program, 'a_Position');
+  gl.vertexAttribPointer(a_Position, 3, FLOAT, false, 
+      vertices.elementSizeInBytes * 3, 0);
+  gl.enableVertexAttribArray(a_Position);
+  
+  Buffer colorBuffer = gl.createBuffer();
+  gl.bindBuffer(ARRAY_BUFFER, colorBuffer);
+  gl.bufferDataTyped(ARRAY_BUFFER, colors, STATIC_DRAW);
+  
+  int a_Color = gl.getAttribLocation(program, 'a_Color');
+  gl.vertexAttribPointer(a_Color, 3, FLOAT, false,
+      vertices.elementSizeInBytes * 3, 0);
+  gl.enableVertexAttribArray(a_Color);
   
   Buffer indexBuffer  = gl.createBuffer();
   gl.bindBuffer(ELEMENT_ARRAY_BUFFER, indexBuffer);
   gl.bufferData(ELEMENT_ARRAY_BUFFER, indices, STATIC_DRAW);
-  
-  int a_Position = gl.getAttribLocation(program, 'a_Position');
-  gl.vertexAttribPointer(a_Position, 3, FLOAT, false, 
-      vertices.elementSizeInBytes * 6, 0);
-  gl.enableVertexAttribArray(a_Position);
-  
-  int a_Color = gl.getAttribLocation(program, 'a_Color');
-  gl.vertexAttribPointer(a_Color, 3, FLOAT, false,
-      vertices.elementSizeInBytes * 6, vertices.elementSizeInBytes * 3);
-  gl.enableVertexAttribArray(a_Color);
   
   num fovYRadians = PI / 4;
   num aspectRatio = canvas.width / canvas.height;
@@ -100,6 +110,7 @@ void main() {
   Matrix4 viewMatrix = makeViewMatrix(cameraPosition, cameraFocusPosition, upDirection);
   
   Matrix4 modelMatrix = new Matrix4.identity();
+  //modelMatrix.rotateY(PI );
   
   UniformLocation u_MvpMatrix = gl.getUniformLocation(program, 'u_MvpMatrix');
   Matrix4 mvpMatrix = projMatrix * viewMatrix * modelMatrix;
