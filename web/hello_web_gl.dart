ca623024d7204771c9ca1257c2cca413b72c007f
Ash Oakenfold <AOakenfold@ea.com>
2014-02-03 23:48:49 -0800

    exercise 27: rotating cube with per fragment point light

--- web/hello_web_gl.dart
+++ web/hello_web_gl.dart
@@ -8,43 +8,57 @@ String vertexShaderSource = '''
 attribute vec4 a_Position;
 attribute vec4 a_Color;
 attribute vec4 a_Normal;
+
 uniform mat4 u_MvpMatrix;
 uniform mat4 u_ModelMatrix;
 uniform mat4 u_NormalMatrix;
-uniform vec3 u_LightColor;
-uniform vec3 u_LightPosition;
-uniform vec3 u_AmbientLightColor;
+
 varying vec4 v_Color;
+varying vec3 v_Position;
+varying vec3 v_Normal;
 
 void main() {
   gl_Position = u_MvpMatrix * a_Position;
   
-  vec3 normal = normalize(vec3(u_NormalMatrix * a_Normal));
-  vec3 vertexInWorldSpace = vec3(u_ModelMatrix * a_Position);
+  v_Color = a_Color;
   
+  // position in world spaace
+  v_Position = vec3(u_ModelMatrix * a_Position);
+
+  v_Normal = normalize(vec3(u_NormalMatrix * a_Normal));
+}
+''';
+
+String fragmentShaderSource = '''
+precision mediump float;
+
+uniform vec3 u_LightColor;
+uniform vec3 u_LightPosition;
+uniform vec3 u_AmbientLightColor;
+
+varying vec4 v_Color;
+varying vec3 v_Position;
+varying vec3 v_Normal;
+
+void main() {
+
+  vec3 normal = normalize(v_Normal);
+
   // unlike directional light (parallel rays), direction of the point light varies by position
-  vec3 pointLightDirection = normalize(u_LightPosition - vertexInWorldSpace);
+  vec3 pointLightDirection = normalize(u_LightPosition - v_Position);
 
   // dot product of light direction against orientation of surface (normal)
   // a negative dot product means θ is more than 90° and light is hitting the back of the surface
   float nDotL = max(dot(pointLightDirection, normal), 0.0);
 
   // take the surface color and mixin light color with intensity determined by angle
-  vec3 diffuseColor = u_LightColor * a_Color.rgb * nDotL;
+  vec3 diffuseColor = u_LightColor * v_Color.rgb * nDotL;
   
   // take the surface color and mix with ambient color
-  vec3 ambientColor = u_AmbientLightColor * a_Color.rgb;
+  vec3 ambientColor = u_AmbientLightColor * v_Color.rgb;
 
   // add colors together for final surface value
-  v_Color = vec4(diffuseColor + ambientColor, a_Color.a);
-}
-''';
-
-String fragmentShaderSource = '''
-precision mediump float;
-varying vec4 v_Color;
-void main() {
-  gl_FragColor = v_Color;
+  gl_FragColor = vec4(diffuseColor + ambientColor, v_Color.a);
 }
 ''';
 
